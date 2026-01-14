38a19228ab7484294562164efb727b1e723c055a
Hanzala Tafzeel <23f2004513@ds.study.iitm.ac.in>
2026-01-14 14:53:37 +0000

    cors

--- api/main.py
+++ api/main.py
@@ -12,8 +12,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
+    allow_credentials=False,  # 🚨 THIS IS THE KEY
 )
 
 # Load telemetry safely
